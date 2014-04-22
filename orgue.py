d1beb71f4cb86dbe1ce5227f0d1ea99f06cff7a7
Baptiste Labat <baptiste.labat@gmail.com>
2014-04-22 12:06:13 +0200

    Suppress display

--- orgue.py
+++ orgue.py
@@ -7,14 +7,17 @@ cam = Camera()
 imgVelleda = cam.getImage()
 imgVelleda = imgVelleda.resize(100, 100)
 
-disp = Display((640, 480))
-wb = imgVelleda.hueDistance(color=Color.BLUE).invert().threshold(210)
-while disp.isNotDone:
+toDisplay = False
+if toDisplay:
+  disp = Display((640, 480))
+
+while True:
     for i_time in range(0, imgVelleda.width) : 
         imgVelleda = cam.getImage().resize(100, 100)
         wb = imgVelleda.hueDistance(color=Color.BLUE).invert().threshold(210)
         # Move a vertical scan line and take blue pixel index
         blue_pix = [i for i in range(len(wb.getVertScanline(i_time))) if wb.getVertScanline(i_time)[i].any()]
+        print blue_pix
         # Draw the scan line
         imgVelleda_copy = imgVelleda.copy()
         imgVelleda_copy.drawLine((i_time,0), (i_time, imgVelleda.height), color= Color.RED)
@@ -22,5 +25,5 @@ while disp.isNotDone:
         # Draw circles at blue point
         for pix in blue_pix:
             imgVelleda_copy.drawCircle((i_time, pix), 5)
-            
-        imgVelleda_copy.save(disp)
+        if toDisplay :
+          imgVelleda_copy.save(disp)
