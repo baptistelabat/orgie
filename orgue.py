62c6dc0a2be5403a9d7e77425a3bc099a328c652
Baptiste Labat <baptiste.labat@gmail.com>
2014-04-23 15:15:43 +0200

    Increase speed

--- orgue.py
+++ orgue.py
@@ -13,17 +13,18 @@ if toDisplay:
 
 while True:
     for i_time in range(0, imgVelleda.width) : 
-        imgVelleda = cam.getImage().resize(100, 100)
-        wb = imgVelleda.hueDistance(color=Color.BLUE).invert().threshold(210)
+        imgVelleda = cam.getImage().resize(100,100)
+        wb = imgVelleda.crop(i_time, 0, 1, imgVelleda.height).hueDistance(color=Color.BLUE).invert().threshold(210).getGrayNumpy()
         # Move a vertical scan line and take blue pixel index
-        blue_pix = [i for i in range(len(wb.getVertScanline(i_time))) if wb.getVertScanline(i_time)[i].any()]
+        blue_pix = [i for i in range(len(wb[0])) if wb[0][i].any()]
         print blue_pix
-        # Draw the scan line
-        imgVelleda_copy = imgVelleda.copy()
-        imgVelleda_copy.drawLine((i_time,0), (i_time, imgVelleda.height), color= Color.RED)
-        imgVelleda_copy.drawCircle( ((0.5+math.floor(i_time*8/imgVelleda.width))*imgVelleda.width/8.0, 10), 5, color=Color.RED, thickness = -1)
-        # Draw circles at blue point
-        for pix in blue_pix:
-            imgVelleda_copy.drawCircle((i_time, pix), 5)
         if toDisplay :
-          imgVelleda_copy.save(disp)
+            # Draw the scan line
+            imgVelleda_copy = imgVelleda.copy()
+            imgVelleda_copy.drawLine((i_time,0), (i_time, imgVelleda.height), color= Color.RED)
+            imgVelleda_copy.drawCircle( ((0.5+math.floor(i_time*8/imgVelleda.width))*imgVelleda.width/8.0, 10), 5, color=Color.RED, thickness = -1)
+            # Draw circles at blue point
+            for pix in blue_pix:
+                imgVelleda_copy.drawCircle((i_time, pix), 5)
+        
+            imgVelleda_copy.save(disp)
